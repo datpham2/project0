811ba869dc06280bd5c234ae658a8fd9e7f9a1ae
datpham2 <0306181218@caothang.edu.vn>
2021-12-27 17:15:33 +0700

    Add event listener to task input field

---
+++ script.js
@@ -0,0 +1,41 @@
+const classNames = {
+  TODO_ITEM: 'todo-container',
+  TODO_CHECKBOX: 'todo-checkbox',
+  TODO_TEXT: 'todo-text',
+  TODO_DELETE: 'todo-delete',
+}
+
+const list = document.getElementById('todo-list')
+const itemCountSpan = document.getElementById('item-count')
+const uncheckedCountSpan = document.getElementById('unchecked-count')
+
+function newTodo() {
+
+  // Find me the button with a class of button
+  const button = document.querySelector('.button');
+
+  // Create a form element
+  const form = document.createElement('form');
+
+  // Create a class for the form
+  form.className = 'task_editor center';
+
+  // Create an input fill inside of form using innerHTML property
+  form.innerHTML = '<textarea id="task" cols="50" name="task" placeholder="New Task" rows="4"></textarea><br><input disabled type="submit" value="Add task">';
+
+  // Find me the parent of an existing child
+  const container = document.querySelector('.container');
+
+  // Insert the form as a child, right before an existing child
+  container.insertBefore(form, button);
+
+  // Hide the New TODO button
+  button.style.display = 'none';
+
+  // Add event listener to task input field
+  const taskInput = document.querySelector('#task');
+  const addTaskButton = document.querySelector('')
+  taskInput.onkeyup = () => {
+    
+  }
+}
